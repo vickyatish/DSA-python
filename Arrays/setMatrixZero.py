c24a0b9867bf35392131fba52f119b33968c7862
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2025-02-22 09:05:21 -0800

    setMatrixZero.py added

--- Arrays/setMatrixZero.py
+++ Arrays/setMatrixZero.py
@@ -1,20 +1,34 @@
-matrix = [[1,1,0],[1,0,1],[1,1,1]]
+def setMatrixZero(matrix, n, m):
+    col0 = 1
 
-r, c = len(matrix), len(matrix[0])
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
 
-row = [1 for i in range(len(matrix))]
-col = [1 for i in range(len(matrix[0]))]
+                if j!=0:
+                    matrix[0][j] = 0
+                else:
+                    col0 = 0
+    
+    for i in range(1, n):
+        for j in range(1, m):
+            if matrix[i][j] != 0:
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
 
-for i in range(r):
-    for j in range(c):
-        if matrix[i][j] == 0:
-            row[i] = 0
-            col[j] = 0
-        
-for i in range(r):
-    for j in range(c):
-        if row[i]==0 or col[j]==0:
-            matrix[i][j]=0
+    if matrix[0][0] == 0:
+        for j in range(m):
+            matrix[0][j] = 0
+    if col0 == 0:
+        for i in range(n):
+            matrix[i][0] = 0
 
-for i in range(len(matrix)):
-    print(matrix[i], end="\n")
+if __name__ == "__main__":
+    matrix = [[1,1,0,1],[1,0,1,1],[1,1,1,1]]
+    m = len(matrix[0])
+    n = len(matrix)
+    setMatrixZero(matrix, n, m)
+    for i in range(len(matrix)):
+        print(matrix[i], end="\n")
+    print("")
