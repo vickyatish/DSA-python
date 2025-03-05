80d89e2b7659d87e1bf059e60ab934b42c6d1282
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2025-03-04 23:21:08 -0800

    twoSum.py

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -8,5 +8,19 @@ def twoSum(arr, target):
 		else:
 			hashmap.add(a)
 
-result = twoSum([1,2,3,4,5], 7)
+def twoSum2(arr, target):
+
+	left, right = 0, len(arr)-1
+
+	while left<right:
+		if arr[left]+arr[right]<target:
+			left+=1
+		elif arr[left]+arr[right]>target:
+			right-=1
+		else:
+			print ([arr[left], arr[right]])
+			left+=1
+	return
+
+result = twoSum2([1,2,3,4,5], 7)
 print(result)
