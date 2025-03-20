63eff924dd71074c4d9f6f1596b50f03bdeb3a5e
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2025-03-19 22:19:08 -0700

    lcs.py added

---
+++ Dynamic Programming/lcs.py
@@ -0,0 +1,19 @@
+def LCS(arr1, arr2):
+    m = len(arr1)
+    n = len(arr2)
+    
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if arr1[i-1] == arr2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+    print(dp[-1][-1])
+
+arr1 = "AGGTAB"
+arr2 = "GXTXAYB"
+
+LCS(arr1, arr2)
