71e7b30de62eb26b1e3bdf7f6050b5c474e97826
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2025-03-28 23:18:16 -0700

    added ./

---
+++ Graph/DFS.py
@@ -0,0 +1,23 @@
+def dfs(adj_list, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=' ')
+
+    for node in adj_list.get(start, []):
+        if node not in visited:  # Fixed typo: 'visted' -> 'visited'
+            dfs(adj_list, node, visited)
+
+def main():
+    adj_list = {
+        0: [1, 2],
+        1: [3],
+        2: [4],
+        3: [],
+        4: [0]
+    }
+    dfs(adj_list, 0)
+
+if __name__ == '__main__':
+    main()
